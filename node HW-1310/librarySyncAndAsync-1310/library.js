56a58914eb9b59571112b77ea02e499aeed9ed39
galatanw <galatanw@icloud.com>
2021-10-18 14:11:08 +0300

    daily

--- node HW-1310/librarySyncAndAsync-1310/library.js
+++ node HW-1310/librarySyncAndAsync-1310/library.js
@@ -1,22 +1,18 @@
-// // שאלה 1
 const fs=require('fs')
 const functions = require("./module")
-    let data=fs.readFileSync("./librarySyncAndAsync/libraryData.txt",'utf-8')
+let data=fs.readFileSync("C:/Users/ester/OneDrive/שולחן העבודה/NODE.JS/node HW-1310/librarySyncAndAsync-1310/libraryData.txt",'utf-8')
 
 
 data=JSON.parse(data)
 console.log(data);
 functions.update(data)
 functions.add(data,'bookZ','GAL',200)
-functions.delete(data,'bookZ')
+functions.delete(data,'bookT')
 console.log(data);
-fs.writeFileSync('./librarySyncAndAsync/libraryData.txt',JSON.stringify(data))
+fs.writeFileSync("C:/Users/ester/OneDrive/שולחן העבודה/NODE.JS/node HW-1310/librarySyncAndAsync-1310/libraryData.txt",JSON.stringify(data))
 
 
 
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// שאלה 2
 
 
 
---
+++ web-server/expressServer/ExpresswithFile/hello_express_files.js
@@ -0,0 +1,15 @@
+const chalk=require('chalk')
+console.log('app is loading');
+const express=require('express');
+const app=express();
+const PORT=8080;
+app.listen(PORT)
+console.log('after listening');
+app.get("/",(req,res)=>{
+    console.log(req.url);
+    res.send('<h1> index </h1>')
+})
+app.get("/about",(req,res)=>{
+    res.send('<h1> about </h1>')
+})
+
