c63c8bbf7fa4570dc28ed867fe51c72b58229c31
galatanw <buisness.galatanw@gmail.com>
2021-11-25 00:34:45 +0200

    homeWork

--- REACT/app/src/App.js
+++ REACT/app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-    <Register/>
+      <Register/>
     </div>
   );
 }
