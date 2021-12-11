c955fe70257402051f4639c829510cdaf34350f3
galatanw <buisness.galatanw@gmail.com>
2021-12-11 21:08:48 +0200

    pause

--- REACT/app/src/App.js
+++ REACT/app/src/App.js
@@ -1,10 +1,30 @@
 import { Component } from 'react';
 import './App.css';
+import MemoryGame from './components/Game/MemoryGame';
 class App extends Component {
-  render(){
-  return (
-    <div className="App">
+  state={startGame:false,array:[]}
  
+startMemoryGame=(e)=>{  
+ e.preventDefault()
+  const temp=[]
+  for (let index = 0; index < Number(e.target.select.value); index++) {
+  temp.push(index+1)
+}
+this.setState({startGame:true,array:temp})
+}
+  render(){
+    return (
+    <div className="App">
+      <form onSubmit={this.startMemoryGame}>
+      <select name='select'>
+        <option value="3" key="98">6</option>
+        <option value="6" key="99">12</option>
+        <option value="12" key="100">24</option>
+      </select>
+        <input type="submit" value={"start"} />
+      </form>
+      
+    {this.state.startGame?<MemoryGame cards={this.state.array}/>:null}
     </div> 
   );
 }
